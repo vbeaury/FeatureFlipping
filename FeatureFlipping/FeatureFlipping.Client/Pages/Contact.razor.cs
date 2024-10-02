bbdc319edec3864181ceafe0c5ffb8c410ce4d1d
vbeaury <vbeaury@sqli.com>
2024-10-02 16:42:30 +0200

    Redirect if feature not enabled

--- FeatureFlipping/FeatureFlipping.Client/Pages/Contact.razor.cs
+++ FeatureFlipping/FeatureFlipping.Client/Pages/Contact.razor.cs
@@ -1,7 +1,18 @@
 using Microsoft.AspNetCore.Components;
+using Shared.Services;
 
 namespace FeatureFlipping.Client.Pages;
 
 public partial class Contact : ComponentBase
 {
+    [Inject] public FeatureService FeatureService { get; set; } = default!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (!await FeatureService.IsFeatureEnabledAsync("ContactForm"))
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
 }
