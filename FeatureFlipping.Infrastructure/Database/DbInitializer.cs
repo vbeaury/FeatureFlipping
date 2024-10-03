fce5ca2666bc78f69f1a3a388ccdaa407822013a
vbeaury <vbeaury@sqli.com>
2024-10-03 14:57:13 +0200

    Add filter on car list

--- FeatureFlipping.Infrastructure/Database/DbInitializer.cs
+++ FeatureFlipping.Infrastructure/Database/DbInitializer.cs
@@ -23,9 +23,9 @@ public class DbInitializer
 
         var cars = new Car[]
         {
-            new() { Model = "Corolla", Color = "Red", Year = 2020, ManufacturerId = manufacturers[0].Id },
-            new() { Model = "Mustang", Color = "Blue", Year = 2019, ManufacturerId = manufacturers[1].Id },
-            new() { Model = "X5", Color = "Black", Year = 2021, ManufacturerId = manufacturers[2].Id }
+            new() { Model = "Corolla", Color = "Red", Year = 2020, LicensePlate = "ABC123", ManufacturerId = manufacturers[0].Id },
+            new() { Model = "Mustang", Color = "Blue", Year = 2019, LicensePlate = "XYZ789", ManufacturerId = manufacturers[1].Id },
+            new() { Model = "X5", Color = "Black", Year = 2021, LicensePlate = "LMN456", ManufacturerId = manufacturers[2].Id }
         };
 
         context.Cars.AddRange(cars);
--- FeatureFlipping/FeatureFlipping.Client/Pages/CarList.razor.cs
+++ FeatureFlipping/FeatureFlipping.Client/Pages/CarList.razor.cs
@@ -9,9 +9,30 @@ public partial class CarList : ComponentBase
     [Inject] private ICarService CarService { get; set; } = null!;
     
     private List<Car>? _cars;
+    private List<Car>? _allCars;
 
     protected override async Task OnInitializedAsync()
     {
-        _cars = await CarService.GetCarsAsync();
+        _allCars = await CarService.GetCarsAsync();
+        _cars = _allCars;
+    }
+    
+    private void ApplyFilter(ChangeEventArgs e)
+    {
+        var filter = e.Value?.ToString();
+
+        if (filter == null)
+        {
+            _cars = _allCars;
+        }
+        else
+        {
+            _cars = string.IsNullOrWhiteSpace(filter) ? 
+                _allCars : 
+                _allCars?.Where(c => c.LicensePlate?.Contains(filter, StringComparison.OrdinalIgnoreCase) == true)
+                    .ToList();
+        }
+        
+        StateHasChanged();
     }
 }
